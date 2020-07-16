dc34a4f437a0174af8f437abd73add2cf876d23c
luster02 <bombolti_02@hotmail.com>
2020-07-15 22:05:18 -0500

    [ADD]: products page

---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default () => {
+    return (
+        <div className="bg-gray-200">
+            <div className="relative bg-gray-800 md:pt-32 pb-32 pt-12">
+                <div className="px-4 md:px-10 mx-auto w-full">
+
+                </div>
+            </div>
+            <div className="">
+                <span className="rounded-full py-2 px-3 w-10 h-10 bg-gray-900 absolute bottom-0 right-0">
+                    <i className="fas fa-plus text-white"></i>
+                </span>
+            </div>
+        </div>
+    )
+}
